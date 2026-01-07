9c3ff76afa1b0b13e93a70fee513e965ec96e087
Petra Johansson <petra.j-son@outlook.com>
2026-01-07 19:44:47 +0100

    started project

---
+++ src/test/java/com/petra/final_exam_work/FinalExamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petra.final_exam_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
