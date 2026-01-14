ff162c604e2e97aa41a54db93f517c74b46c0081
Petra Johansson <petra.j-son@outlook.com>
2026-01-14 16:07:44 +0100

    Entity ConsentForm

---
+++ src/main/java/com/petra/final_exam_work/Entity/ConsentForm.java
@@ -0,0 +1,98 @@
+package com.petra.final_exam_work.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "consent_forms")
+public class ConsentForm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "public_uuid",
+            updatable = false,
+            unique = true,
+            nullable = false
+    )
+    private UUID publicUuid;
+
+    @Column(name = "id_card_file_path", nullable = false)
+    private String idCardFilePath;
+
+    @Column(name = "id_face_file_path", nullable = false)
+    private String idFaceFilePath;
+
+    @Column(name = "face_fff_file_path", nullable = false)
+    private String facefffFilePath;
+
+    @Column(name = "approve_rules", nullable = false)
+    private Boolean approvedRules;
+
+    @OneToMany(mappedBy = "consentForm", cascade =CascadeType.ALL, orphanRemoval = true)
+    private Set<UserConsentForm> users = new HashSet<>();
+
+    public ConsentForm() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UUID getPublicUuid() {
+        return publicUuid;
+    }
+
+    public void setPublicUuid(UUID publicUuid) {
+        this.publicUuid = publicUuid;
+    }
+
+    public String getIdCardFilePath() {
+        return idCardFilePath;
+    }
+
+    public void setIdCardFilePath(String idCardFilePath) {
+        this.idCardFilePath = idCardFilePath;
+    }
+
+    public String getIdFaceFilePath() {
+        return idFaceFilePath;
+    }
+
+    public void setIdFaceFilePath(String idFaceFilePath) {
+        this.idFaceFilePath = idFaceFilePath;
+    }
+
+    public String getFacefffFilePath() {
+        return facefffFilePath;
+    }
+
+    public void setFacefffFilePath(String facefffFilePath) {
+        this.facefffFilePath = facefffFilePath;
+    }
+
+    public Boolean getApprovedRules() {
+        return approvedRules;
+    }
+
+    public void setApprovedRules(Boolean approvedRules) {
+        this.approvedRules = approvedRules;
+    }
+
+    public Set<UserConsentForm> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserConsentForm> users) {
+        this.users = users;
+    }
+}
