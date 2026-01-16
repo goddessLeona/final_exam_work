607c9bc7a44572c9da938e7ed3f48c831b173d6a
Petra Johansson <petra.j-son@outlook.com>
2026-01-16 19:37:54 +0100

    Entity Tag

---
+++ src/main/java/com/petra/final_exam_work/Entity/Tag.java
@@ -0,0 +1,63 @@
+package com.petra.final_exam_work.Entity;
+
+import com.petra.final_exam_work.Entity.photo.PhotoAlbum;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            name = "public_uuid",
+            updatable = false,
+            unique = true,
+            nullable = false
+    )
+    private UUID publicUuid;
+
+    @Column(name = "name_tag", nullable = false, unique = true)
+    private String nameTag;
+
+    @ManyToMany
+    @JoinTable(
+            name = "photo_albums_tags",
+            joinColumns = @JoinColumn(name = "tag_id"),
+            inverseJoinColumns = @JoinColumn(name = "photo_album_id")
+    )
+    private Set<PhotoAlbum> photoAlbums = new HashSet<>();
+
+    public Tag() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UUID getPublicUuid() {
+        return publicUuid;
+    }
+
+    public void setPublicUuid(UUID publicUuid) {
+        this.publicUuid = publicUuid;
+    }
+
+    public String getNameTag() {
+        return nameTag;
+    }
+
+    public void setNameTag(String nameTag) {
+        this.nameTag = nameTag;
+    }
+}
