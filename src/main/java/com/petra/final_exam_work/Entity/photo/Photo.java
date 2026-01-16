fe503f21a4d42e5e303e52237e8f11766a2d138d
Petra Johansson <petra.j-son@outlook.com>
2026-01-16 12:18:08 +0100

    Entity Photo

--- src/main/java/com/petra/final_exam_work/Entity/photo/Photo.java
+++ src/main/java/com/petra/final_exam_work/Entity/photo/Photo.java
@@ -1,5 +1,6 @@
-package com.petra.final_exam_work.Entity;
+package com.petra.final_exam_work.Entity.photo;
 
+import com.petra.final_exam_work.Entity.user.User;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 
