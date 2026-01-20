4d34ff838ff647310acaf5b004f55797aa1f238e
Petra Johansson <petra.j-son@outlook.com>
2026-01-20 17:50:36 +0100

    updadted footer

---
+++ frontend/src/app/contributor/page.tsx
@@ -0,0 +1,11 @@
+export default function ContributorPage() {
+  return (
+    <div>
+      <main>
+        <p>Welcom new contributor</p>
+        <p>We are happy to have you part of our pages</p>
+        <p>Before you can be part of the comunity and contribute with content, you have to fill in this form</p>
+      </main>
+    </div>
+  );
+}
