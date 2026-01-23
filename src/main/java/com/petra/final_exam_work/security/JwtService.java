1537de590e3f7d6fbd1d2ba7d757d75fcfa1be05
Petra Johansson <petra.j-son@outlook.com>
2026-01-23 20:47:19 +0100

    continued adding needed files for getting login to work.

---
+++ src/main/java/com/petra/final_exam_work/security/JwtService.java
@@ -0,0 +1,76 @@
+package com.petra.final_exam_work.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    // Must be at least 256 bits for HS256
+    private static final String SECRET_KEY =
+            "ivseL/+tgTDjx+ZH1OrM7R9Ds0yfeIb+ya3FUK5XBY8=";
+
+    // expire after 1h
+    private static final long EXPERATION_TIME = 1000 * 60 * 60;
+
+    //public API- coming
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTolkenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPERATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //internal helpers
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(
+            String token,
+            Function<Claims, T> claimsResolver
+    ){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+     private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+     }
+}
