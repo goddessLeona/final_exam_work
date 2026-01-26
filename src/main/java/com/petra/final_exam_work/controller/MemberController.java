83e7ccf3255351c067b5f5dd894a5f4ba2b5af3f
Petra Johansson <petra.j-son@outlook.com>
2026-01-26 20:37:22 +0100

    continued adding needed files for getting login to work.

---
+++ src/main/java/com/petra/final_exam_work/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.petra.final_exam_work.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/test")
+    public String memberTest(){
+        return "Hello MEMBER, you are now authenticated !";
+    }
+}
